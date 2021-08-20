884914d3019a1772bfe999231cdcb8cf1835bd37
YaroslavKnysh <81957031+YaroslavKnysh@users.noreply.github.com>
2021-08-20 15:42:37 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+import './js/module1-variables.js';
+import './js/module1-primitives.js';
+import './js/module1-toString.js';
+import './js/module1-toNumber.js';
+import './js/module1-toBoolean.js';
+import './js/false-values.js';
+import './js/lesson4.js';
