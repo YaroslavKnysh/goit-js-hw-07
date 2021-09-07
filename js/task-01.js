e75700a0c19824522610107cc1012f8e5220ad82
YaroslavKnysh <yarik.knysh@gmail.com>
2021-09-07 15:17:13 +0300

    Задания + комменты

--- js/task-01.js
+++ js/task-01.js
@@ -7,3 +7,15 @@ for (let i = 0; i < ulEl.length; i++) {
   console.log('Категория:', item.firstElementChild.textContent);
   console.log('Количесвто элементов:', item.lastElementChild.children.length);
 }
+
+// Привет! Таск_1
+// Когда ты начнаешь использовать firstElementChild или astElementChild.children. ты что-то делаешь не так. Задумайся.
+
+// const list = document.querySelector('#categories');
+// const items = list.querySelectorAll('.item');
+// console.log(`В списке ${items.length} категории:`);
+// items.forEach(node => {
+// const h2 = node.querySelector('h2');
+// const nodeItems = node.querySelectorAll('li');
+// console.log(` - категория: ${h2.textContent} (количество элементов: ${nodeItems.length})`);
+// });
